ecac87863f64c3d01802f19a2e3879fda0898905
DennerBismarck <dennerbismarck@gmail.com>
2023-04-21 11:48:42 -0300

    Feat: Update Função

--- src/routes/funcao.routes.ts
+++ src/routes/funcao.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { CreateFuncaoController } from "../controller/FuncaoController/createFuncao";
 import { ReadFuncaoController } from "../controller/FuncaoController/readFuncao";
+import { updateFuncaoController } from "../controller/FuncaoController/updateFuncao";
 
 const funcaoRoutes = Router();
 
@@ -9,5 +10,6 @@ const ReadFuncao = new ReadFuncaoController();
 
 funcaoRoutes.post("/create", CreateFuncao.handler);
 funcaoRoutes.post("/read", ReadFuncao.handler);
+funcaoRoutes.post("/update/:id", updateFuncaoController);
 
 export {funcaoRoutes};
