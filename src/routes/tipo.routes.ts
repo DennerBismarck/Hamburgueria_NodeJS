405585fe4a5283da770a1daf34d67af868eecc9d
DennerBismarck <dennerbismarck@gmail.com>
2023-04-21 12:41:29 -0300

    Feat: Delete Tipos #10

--- src/routes/tipo.routes.ts
+++ src/routes/tipo.routes.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { CreateTipoController } from "../controller/TipoController/createTipo";
 import { ReadTipoController } from "../controller/TipoController/readTipo";
 import { updateTipoController } from "../controller/TipoController/updateTipo";
+import { deleteTipoController } from "../controller/TipoController/deletetipo";
+
 
 const tipoRoutes = Router();
 
@@ -11,5 +13,6 @@ const ReadTipo = new ReadTipoController();
 tipoRoutes.post("/create", CreateTipo.handler);
 tipoRoutes.post("/read", ReadTipo.handler);
 tipoRoutes.post("/update/:id", updateTipoController);
+tipoRoutes.post("/delete/:id", deleteTipoController);
 
 export{tipoRoutes}
